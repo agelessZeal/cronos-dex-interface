53046bb60221a3e4a24c20c75b9a270d8b3233bc
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2021-11-18 21:00:39 +0200

    change LP permit name

--- src/hooks/useERC20Permit.ts
+++ src/hooks/useERC20Permit.ts
@@ -268,7 +268,7 @@ export function useERC20Permit(
 
 const REMOVE_V2_LIQUIDITY_PERMIT_INFO: PermitInfo = {
   version: '1',
-  name: 'OlympusGate LP Token',
+  name: 'OlympusGate LPs',
   type: PermitType.AMOUNT,
 }
 
